b32dd511a6e60afa22d4e8d639be195c296004dc
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-08-17 19:52:04 +0200

    app functionality done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import Data from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState([""]);
+
   return (
     <div className="App">
-      <input type="text" name="search" placeholder="Search" />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <div className="data">
+        {Data.filter((item) => {
+          if (query == "") {
+            return item;
+          } else if (
+            item.first_name.toLowerCase().includes(query.toLowerCase())
+          ) {
+            return item;
+          }
+        }).map((item) => {
+          return <p key={item.id}>{item.first_name}</p>;
+        })}
+      </div>
     </div>
   );
 }
